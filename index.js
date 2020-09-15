dc0ee701adf80bd49dd18a7c9a128e716aac166d
o96ra244 <o.tsuyoshi823@gmail.com>
2020-09-15 18:19:46 +0900

    レンダラーを作成

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.addEventListener("DOMContentLoaded", init);
 
-function init() {}
+function init() {
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas")
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+}
